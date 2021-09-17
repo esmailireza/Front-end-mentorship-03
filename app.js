b55c9c29e4afde2f94b9bf9fec0ff1543e30693b
Your Name <you@example.com>
2021-09-17 20:32:41 +0430

    created project

---
+++ app.js
@@ -0,0 +1,6 @@
+/* document.addEventListener("DOMContentLoaded", changeDate);
+
+function changeDate() {
+  console.log("fgdkfskfjd");
+  document.querySelector(".date").innerText = "addewfewefw";
+} */
